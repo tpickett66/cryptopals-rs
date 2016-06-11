96f9d8eba15969ca069227b49e242c0601782337
Tyler Pickett <t.pickett66@gmail.com>
2016-06-10 22:00:52 -0600

    Solve Set 1 Problem 6

---
+++ examples/xor_crack.rs
@@ -0,0 +1,68 @@
+extern crate rustc_serialize;
+extern crate crypto_challenges;
+
+use std::fs::File;
+use std::io::prelude::*;
+
+use self::rustc_serialize::base64::FromBase64;
+
+use crypto_challenges::etaoin_shrdlu_score;
+use crypto_challenges::byte_array_hamming_distance;
+use crypto_challenges::xor_cipher_byte_decrypt as decrypt;
+use crypto_challenges::repeating_xor_cipher_decrypt as full_decrypt;
+fn main() {
+    let mut f = File::open("data/6.txt").unwrap();
+    let mut buffer = String::new();
+
+    f.read_to_string(&mut buffer).unwrap();
+    let ciphertext = buffer.as_str().from_base64().unwrap();
+
+    let possible_key_sizes = 2..41;
+    let mut distances: Vec<(usize, u64)> = vec![];
+    for possible_key_size in possible_key_sizes {
+         let total_score = ciphertext.
+            chunks(possible_key_size).
+            collect::<Vec<&[u8]>>().
+            windows(2).
+            filter(|pair| pair[0].len() == pair[1].len()).
+            map(|pair| byte_array_hamming_distance(pair[0], pair[1])).
+            fold(0, |a, b| a + b);
+        distances.push((possible_key_size, total_score));
+    }
+
+    distances.sort_by(|a, b| a.1.cmp(&b.1));
+
+    let keysize = distances[0].0;
+    println!("Chosen Key Size: {}", keysize);
+
+    let mut transposed_ciphertext: Vec<Vec<u8>> = (0..keysize).map(|_| vec![]).collect();
+    for chunk in ciphertext.chunks(keysize) {
+        for (idx, byte) in chunk.iter().enumerate() {
+            transposed_ciphertext[idx].push(byte.clone());
+        }
+    }
+
+    let mut key_bytes: Vec<u8> = vec![];
+
+    for block in transposed_ciphertext.iter() {
+        let mut key_scores: Vec<(u8, usize)> = vec![];
+        let keys = 0..255;
+
+        for key in keys {
+            let result = decrypt(key, block.as_slice());
+            match String::from_utf8(result) {
+                Ok(val) => {
+                    key_scores.push((key, etaoin_shrdlu_score(&val)));
+                },
+                Err(_) => {}
+            }
+        }
+        key_scores.sort_by(|a, b| b.1.cmp(&a.1));
+        key_bytes.push(key_scores[0].0);
+    }
+
+    let key = String::from_utf8(key_bytes.clone()).unwrap();
+    println!("Probable Key: \"{}\"", key);
+    let plaintext = full_decrypt(key_bytes.as_slice(), ciphertext.as_slice()).unwrap();
+    println!("Plaintext:\n{}", plaintext);
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,9 @@
 extern crate rustc_serialize;
 use self::rustc_serialize::hex::FromHex;
 use self::rustc_serialize::hex::ToHex;
-use self::rustc_serialize::base64::ToBase64;
 use self::rustc_serialize::base64::Config as Base64Config;
+use self::rustc_serialize::base64::FromBase64;
+use self::rustc_serialize::base64::ToBase64;
 
 use std::string::FromUtf8Error;
 
@@ -17,6 +18,10 @@ pub fn hex_to_base64(input: &str) -> String {
     hex.map(|byte| byte.to_base64(config)).unwrap()
 }
 
+pub fn base64_to_hex(input: &str) -> String {
+    input.from_base64().unwrap().to_hex()
+}
+
 pub fn fixed_xor(lhs: &str, rhs: &str) -> String {
     let lhs_bytes = lhs.from_hex().unwrap();
     let rhs_bytes = rhs.from_hex().unwrap();
@@ -28,22 +33,16 @@ pub fn fixed_xor(lhs: &str, rhs: &str) -> String {
     result_vec.to_hex()
 }
 
-pub fn xor_cipher_decrypt(key: u8, ciphertext: &str) -> Result<String, FromUtf8Error> {
-    let ciphertext_bytes = ciphertext.from_hex().unwrap();
-    let result_bytes = ciphertext_bytes.
-        into_iter().
+pub fn xor_cipher_byte_decrypt(key: u8, ciphertext: &[u8]) -> Vec<u8> {
+    ciphertext.into_iter().
         map(|b| b ^ key).
-        collect::<Vec<u8>>();
-    String::from_utf8(result_bytes)
+        collect::<Vec<u8>>()
 }
 
-const ETAOIN_SHRDLU: [char; 13] = ['E', 'T', 'A', 'O', 'I', 'N', ' ', 'S', 'H', 'R', 'D', 'L', 'U'];
-
-pub fn is_likely_message(maybe_message: &String) -> bool {
-    let magic_letters = maybe_message.chars().into_iter().
-        filter(|c| ETAOIN_SHRDLU.contains(&c.to_uppercase().next().unwrap())).
-        collect::<Vec<_>>();
-    magic_letters.len() >= 20
+pub fn xor_cipher_decrypt(key: u8, ciphertext: &str) -> Result<String, FromUtf8Error> {
+    let ciphertext_bytes = ciphertext.from_hex().unwrap();
+    let result_bytes = xor_cipher_byte_decrypt(key, ciphertext_bytes.as_slice());
+    String::from_utf8(result_bytes)
 }
 
 pub fn repeating_xor_cipher_encrypt(key: &str, plaintext: &str) -> String {
@@ -55,11 +54,57 @@ pub fn repeating_xor_cipher_encrypt(key: &str, plaintext: &str) -> String {
     ciphertext_bytes.to_hex()
 }
 
+pub fn repeating_xor_cipher_decrypt(key: &[u8], ciphertext: &[u8]) -> Result<String, FromUtf8Error> {
+    let plaintext_bytes = ciphertext.into_iter().
+        zip(key.iter().cycle().take(ciphertext.len())).
+        map(|bytes| bytes.0 ^ bytes.1).
+        collect::<Vec<u8>>();
+    String::from_utf8(plaintext_bytes)
+}
+
+const ETAOIN_SHRDLU: [char; 13] = ['E', 'T', 'A', 'O', 'I', 'N', ' ', 'S', 'H', 'R', 'D', 'L', 'U'];
+pub fn etaoin_shrdlu_score(scorable: &String) -> usize {
+     scorable.chars().into_iter().
+        filter(|c| ETAOIN_SHRDLU.contains(&c.to_uppercase().next().unwrap())).
+        collect::<Vec<_>>().
+        len()
+}
+
+pub fn is_likely_message(maybe_message: &String) -> bool {
+    etaoin_shrdlu_score(maybe_message) >= 20
+}
+
+pub fn byte_array_hamming_distance(a: &[u8], b: &[u8]) -> u64 {
+    assert_eq!(a.len(), b.len());
+    a.into_iter().
+        zip(b).
+        fold(0, |ones, (a, b)| ones + (a ^ b).count_ones() as u64)
+}
+
+pub fn hamming_distance(a: &str, b: &str) -> u64 {
+    byte_array_hamming_distance(a.as_bytes(), b.as_bytes())
+}
+
+pub fn hex_hamming_distance(a: &str, b: &str) -> u64 {
+    assert_eq!(a.len(), b.len());
+    byte_array_hamming_distance(
+        a.from_hex().unwrap().as_slice(),
+        b.from_hex().unwrap().as_slice()
+    )
+}
+
 #[test]
-fn test_conversion() {
+fn test_hex_to_base64_conversion() {
     let hex = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
     let base64 = hex_to_base64(hex);
     assert!(base64 == "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t");
+}
+
+#[test]
+fn test_base64_to_hex_conversion() {
+    let base64 = "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t";
+    let hex = base64_to_hex(base64);
+    assert!(hex == "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d");
 }
 
 #[test]
@@ -79,4 +124,31 @@ fn test_repeating_xor_cipher_encrypt() {
     println!("Expected Ciphertext: {}", expected_ciphertext);
     println!("         Ciphertext: {}", ciphertext);
     assert!(ciphertext == expected_ciphertext);
+}
+
+#[test]
+fn test_repeating_xor_cipher_decrypt() {
+    let ciphertext = "0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f";
+    let key = "ICE";
+    let expected_plaintext = "Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal";
+    let plaintext = repeating_xor_cipher_decrypt(key, ciphertext).unwrap();
+    println!("Expected plaintext: {}", expected_plaintext);
+    println!("         plaintext: {}", plaintext);
+    assert!(plaintext == expected_plaintext);
+}
+
+#[test]
+fn test_hamming() {
+    let a = "this is a test";
+    let b = "wokka wokka!!!";
+    let distance = hamming_distance(a, b);
+    assert_eq!(37, distance);
+}
+
+#[test]
+fn test_hex_hamming() {
+    let a = "3749521a010715114f104f211a632d1f0c4e084e5848264f030a491c0b";
+    let b = "78453102040b411b01522a0856413b521d060654540e104e0516491e10";
+    let distance = hex_hamming_distance(a, b);
+    assert_eq!(77, distance);
 }
