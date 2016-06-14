d87730cfccbc96a9a43aa2b144f45a5ca120f1ef
Tyler Pickett <t.pickett66@gmail.com>
2016-06-14 08:50:36 -0600

    Fix tests broken by previous iteration on solutions.

--- src/lib.rs
+++ src/lib.rs
@@ -131,7 +131,7 @@ fn test_repeating_xor_cipher_decrypt() {
     let ciphertext = "0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f";
     let key = "ICE";
     let expected_plaintext = "Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal";
-    let plaintext = repeating_xor_cipher_decrypt(key, ciphertext).unwrap();
+    let plaintext = repeating_xor_cipher_decrypt(key.as_bytes(), ciphertext.from_hex().unwrap().as_slice()).unwrap();
     println!("Expected plaintext: {}", expected_plaintext);
     println!("         plaintext: {}", plaintext);
     assert!(plaintext == expected_plaintext);
